7b437145c700ecc5141a34e8f4302e1185041b11
Shiwani Shah <shahshiwani246@gmail.com>
2024-02-29 09:20:59 +0545

    files changed

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,10 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
+          "Developer",
           "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "ML Enthusiast"
+        
         ],
         autoStart: true,
         loop: true,
